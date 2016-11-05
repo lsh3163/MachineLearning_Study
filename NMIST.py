2dd7439bd5dcecbe674b6a662f082d9e04d4a44e
lsh3163 <easter3163@naver.com>
2016-11-05 17:25:51 +0900

    Update NMIST.py

--- NMIST.py
+++ NMIST.py
@@ -6,23 +6,23 @@ mnist = input_data.read_data_sets('./samples/MNIST_data/', one_hot=True)
 import tensorflow as tf
 
 x = tf.placeholder(tf.float32, [None, 784])
-//784*10의 행렬값
+#784*10의 행렬값
 W = tf.Variable(tf.zeros([784, 10]))
-//숫자의 종류 개수
+#숫자의 종류 개수
 b = tf.Variable(tf.zeros([10]))
-//matrix 끼리 곱함
+#matrix 끼리 곱함
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
-//Cost Function
+#Cost Function
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 init = tf.initialize_all_variables()
-//One Session 
+#One Session 
 sess = tf.Session()
 sess.run(init)
 for i in range(1000):
-  //쪼개서 여러번 돌림
+  #쪼개서 여러번 돌림
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
