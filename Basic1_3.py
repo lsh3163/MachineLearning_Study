413c47ceac2a141f723e9d3004c2d90a8125a966
lsh3163 <easter3163@naver.com>
2016-11-05 22:41:48 +0900

    Create Basic1_3.py

---
+++ Basic1_3.py
@@ -0,0 +1,9 @@
+#Basic Operation
+import tensorflow as tf
+a = tf.constant(2)
+b = tf.constant(3)
+
+with tf.Session() as sess:
+  print("a=2, b=3")
+  print("Addition with Constants: %i" %sess.run(a+b))
+  print("Multiplication with Constants: %i" % sess.run(a*b))
